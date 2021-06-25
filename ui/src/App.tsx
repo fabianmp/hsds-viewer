73aa2c2f57f6b6515bef95018b35a0f9ad5298b1
Fabian Zach <fabian.zach@methodpark.de>
2021-06-25 11:53:34 +0200

    Deselect domain when changing folders

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -81,6 +81,11 @@ export default function App() {
 
   const handleDrawerToggle = useCallback(() => setDrawerOpen(!drawerOpen), [drawerOpen]);
 
+  const handleSelectFolder = (path: string) => {
+    setSelectedFolderPath(path);
+    setSelectedDomainPath("");
+  }
+
   const classes = useStyles();
   return (
     <Router>
@@ -98,17 +103,17 @@ export default function App() {
             <Hidden lgUp implementation="css">
               <Drawer variant="temporary" className={classes.drawerSmall} classes={{ paper: clsx(classes.drawerSmall, classes.drawerPaperSmall) }}
                 open={drawerOpen} onClose={handleDrawerToggle}>
-                {rootFolder && <FolderTree folder={rootFolder} selectedFolderPath={selectedFolderPath} handleNodeSelect={setSelectedFolderPath} />}
+                {rootFolder && <FolderTree folder={rootFolder} selectedFolderPath={selectedFolderPath} handleNodeSelect={handleSelectFolder} />}
               </Drawer>
             </Hidden>
             <Hidden mdDown implementation="css">
               <Drawer variant="permanent" className={classes.drawerLarge} classes={{ paper: clsx(classes.drawerLarge, classes.drawerPaperLarge) }} open>
-                {rootFolder && <FolderTree folder={rootFolder} selectedFolderPath={selectedFolderPath} handleNodeSelect={setSelectedFolderPath} />}
+                {rootFolder && <FolderTree folder={rootFolder} selectedFolderPath={selectedFolderPath} handleNodeSelect={handleSelectFolder} />}
               </Drawer>
             </Hidden>
             <Grid container>
               <Grid item xs={12} md={8} xl={9} className={classes.column}>
-                {selectedFolder && <FolderCrumbs selectedFolderPath={selectedFolderPath} selectPath={setSelectedFolderPath} />}
+                {selectedFolder && <FolderCrumbs selectedFolderPath={selectedFolderPath} selectPath={handleSelectFolder} />}
                 {selectedFolder && acls.length > 0 && <AccessControl acls={acls} variant="wide" />}
                 {selectedFolder && <FolderContent folder={selectedFolder} handleSelect={setSelectedDomainPath} selected={selectedDomainPath} />}
               </Grid>
